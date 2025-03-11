1e02a137e925d9a162c88043990dddba7626f11e
ollycassidy13 <ollyj.cassidy@gmail.com>
2025-03-11 14:52:01 +0000

    modes code

--- accelerometer_modes.c
+++ accelerometer_modes.c
@@ -105,11 +105,99 @@ int get_command_char(void)
     return ch;
 }
 
+void int_to_str(int num, char *buf) {
+    if (num == 0) {
+        buf[0] = '0';
+        buf[1] = '\0';
+        return;
+    }
+
+    char temp[12];
+    int i = 0;
+    while (num > 0) {
+        temp[i++] = '0' + (num % 10);
+        num /= 10;
+    }
+    // Reverse the digits into the output buffer.
+    int j;
+    for (j = 0; j < i; j++) {
+        buf[j] = temp[i - j - 1];
+    }
+    buf[i] = '\0';
+}
+
+// Custom function to convert a float to a string with a given precision.
+// 'precision' is the number of digits after the decimal point.
+void float_to_string_custom(float num, char *buf, int precision) {
+    char *ptr = buf;
+
+    // Handle negative numbers.
+    if (num < 0) {
+        *ptr++ = '-';
+        num = -num;
+    }
+
+    // Separate the integer part.
+    int int_part = (int)num;
+    char int_buf[12];
+    int_to_str(int_part, int_buf);
+
+    // Copy the integer part into the output buffer.
+    char *p = int_buf;
+    while (*p) {
+        *ptr++ = *p++;
+    }
+
+    // If precision is specified, process the fractional part.
+    if (precision > 0) {
+        *ptr++ = '.';
+        float fractional = num - int_part;
+
+        // Calculate multiplier for the required precision (10^precision).
+        int multiplier = 1;
+        for (int i = 0; i < precision; i++) {
+            multiplier *= 10;
+        }
+
+        // Convert the fractional part to an integer (with rounding).
+        int frac_int = (int)(fractional * multiplier + 0.5f);
+
+        // Convert the fractional integer to string.
+        char frac_buf[12];
+        int_to_str(frac_int, frac_buf);
+
+        // Determine the length of the fractional string.
+        int len = 0;
+        p = frac_buf;
+        while (*p++) {
+            len++;
+        }
+
+        // If the fractional part is shorter than the required precision,
+        // pad with leading zeros.
+        int pad = precision - len;
+        while (pad-- > 0) {
+            *ptr++ = '0';
+        }
+
+        // Copy the fractional part.
+        p = frac_buf;
+        while (*p) {
+            *ptr++ = *p++;
+        }
+    }
+
+    *ptr = '\0';
+}
+
 int main(void)
 {
     alt_putstr("Duel Game\n");
-    alt_32 x_read;
-    alt_u8 out;
+    alt_32 x_read, y_read;
+    float x_value, y_value, x_processed, y_processed;
+    char x_str[32];
+    char y_str[32];
+    // alt_u8 out;
     alt_up_accelerometer_spi_dev * acc_dev;
     acc_dev = alt_up_accelerometer_spi_open_dev("/dev/accelerometer_spi");
     if (acc_dev == NULL) {
@@ -129,11 +217,11 @@ int main(void)
        if (cmd != -1) {
             if (cmd == '0') {
                 mode = 0;
-                alt_printf("Switched to Mode 0 (off).\n");
+//                alt_printf("Switched to Mode 0 (off).\n");
             }
             else if (cmd == '1') {
                 mode = 1;
-                alt_printf("Switched to Mode 1 (filtering).\n");
+//                alt_printf("Switched to Mode 1 (filtering).\n");
             }
            //  else if (cmd == '2') {
            //     mode = 2;
@@ -144,22 +232,27 @@ int main(void)
                 break;
             }
             else {
-                alt_printf("Unrecognized command: %c\n", cmd);
+            	mode = 0;
+//                alt_printf("Unrecognized command: %c\n", cmd);
             }
         }
 
-       if (mode != 0) {
-           if (mode == 1) {
-               alt_up_accelerometer_spi_read_x_axis(acc_dev, &x_read);
-               alt_printf("Raw data: %x\n", x_read);
+//       if (mode != 0) {
+            if (mode == 1) {
+                alt_up_accelerometer_spi_read_x_axis(acc_dev, &x_read);
+                alt_up_accelerometer_spi_read_y_axis(acc_dev, &y_read);
+            //    alt_printf("Raw data: %x\n", x_read);
 
-               float x_value = (float)x_read;
-               float processed_value = x_value;
+                x_value = (float)x_read;
+                x_processed = fir_filter(x_value);
+                y_value = (float)y_read;
+                y_processed = fir_filter(y_value);
 
-               processed_value = fir_filter(x_value);
-               alt_printf("Filtered x: %.2f\n", processed_value);
+                float_to_string_custom(x_processed, x_str, 3);
+                float_to_string_custom(y_processed, y_str, 3);
 
-               convert_read((alt_32)processed_value, &level, &led);
+                    // Use alt_printf to print the converted strings.
+                    alt_printf("%s %s\n", x_str, y_str);
 
                // add the uart return data here
 //                out = *level;
@@ -167,7 +260,7 @@ int main(void)
 //                alt_putstr(std::tostring(processed_value));
 //                alt_printf("Filtered x: %.2f\n", processed_value);
 
-           }
+        //    }
            // else if (mode == 2) {
            //     // idk what we want for calibration at this stage but that goes here
            // }
